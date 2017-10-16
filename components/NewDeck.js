a81bd3ad0464ca3d0e3abe29e9ecfc0d1a2db091
Timothy Moore <moore.tim@outlook.com>
2017-10-16 15:12:07 -0400

    feat: Add basic markup for New Deck view

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+} from 'react-native';
 
 export default class NewDeck extends Component {
   render() {
     return (
-      <View>
-        <Text>New Deck</Text>
-      </View>
+      /**
+       * For how to hide keyboard on touching outside of TextInput, answers had examples
+       * with wrapping TouchableWithoutFeedback around main view and calling Keyboard.dismiss
+       * onPress
+       * https://stackoverflow.com/questions/29685421/react-native-hide-keyboard
+       */
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Text>What is the title of your new deck?</Text>
+          <TextInput style={styles.textInput} placeholder="Deck Title" />
+          <TouchableOpacity>
+            <Text>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    width: 100,
+    borderColor: '#222',
+    borderWidth: StyleSheet.hairlineWidth,
+  },
+});
