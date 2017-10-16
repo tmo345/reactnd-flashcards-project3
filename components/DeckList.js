8847f4abff18a67269eb80bc00994d0c794855e3
Timothy Moore <moore.tim@outlook.com>
2017-10-16 13:05:59 -0400

    fix: Rename DeskList to correct DeckList

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default class DeskList extends Component {
+export default class DeckList extends Component {
   render() {
     return (
       <View>
