eab173b3af58bee91bb631fbcc77b93377ad3048
Timothy Moore <moore.tim@outlook.com>
2017-10-16 13:29:46 -0400

    feat: Add TabBarNavigator with one tab for Decks (component: DeskList)

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import DeckList from './components/DeckList';
 import { Constants } from 'expo';
+import { TabNavigator } from 'react-navigation';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const FlashCardsStatusBar = ({ backgroundColor, ...props }) => (
   <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
@@ -9,12 +11,39 @@ const FlashCardsStatusBar = ({ backgroundColor, ...props }) => (
   </View>
 );
 
+const Tabs = TabNavigator(
+  {
+    DecksList: {
+      screen: DeckList,
+      navigationOptions: {
+        tabBarLabel: 'Decks',
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialCommunityIcons
+            name="cards-outline"
+            size={30}
+            color={tintColor}
+          />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: '#222',
+      style: {
+        height: 56,
+        backgroundColor: '#fff',
+      },
+    },
+  },
+);
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <FlashCardsStatusBar backgroundColor="#222" barStyle="light-content" />
-        <DeckList />
+        <Tabs />
       </View>
     );
   }
