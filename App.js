d5363dcf723acd7bd082d533eb423e09e1eb2847
Timothy Moore <moore.tim@outlook.com>
2017-10-16 13:08:35 -0400

    feat: Add basic StatusBar to App and remove some default styles

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import DeckList from './components/DeckList';
+import { Constants } from 'expo';
+
+const FlashCardsStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+);
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <FlashCardsStatusBar backgroundColor="#222" barStyle="light-content" />
         <DeckList />
       </View>
     );
@@ -15,8 +23,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
