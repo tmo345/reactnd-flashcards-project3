589b0008ee5a1d50af0daf557beb65e85d5076da
Timothy Moore <moore.tim@outlook.com>
2017-10-16 14:08:21 -0400

    feat: Add and display a list of decks with hard coded dummy data

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 
 export default class DeckList extends Component {
   render() {
     return (
-      <View>
-        <Text>DeckList</Text>
+      <View style={{flex: 1}}>
+        <View style={styles.deckList}>
+          <View style={styles.deck}>
+            <Text>udacicards</Text>
+            <Text>{3} cards</Text>
+          </View>
+          <View style={styles.deck}>
+            <Text>udacicards</Text>
+            <Text>{3} cards</Text>
+          </View>
+          <View style={styles.deck}>
+            <Text>udacicards</Text>
+            <Text>{3} cards</Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  deckList: {
+    flex: 1,
+    padding: 20,
+    justifyContent: Platform.OS === 'ios' ? 'flex-end' : 'flex-start'
+  },
+  deck: {
+    alignItems: 'center',
+    padding: 40,
+    justifyContent: 'center',
+    borderColor: '#222',
+    borderWidth: StyleSheet.hairlineWidth,
+
+  }
+})
