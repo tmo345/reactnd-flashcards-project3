fd56cc8612ae4df44a374bc1e58b66a8ac38de24
Timothy Moore <moore.tim@outlook.com>
2017-10-16 22:35:56 -0400

    styles: Add basic styles and layout to IndividualDeck

--- components/IndividualDeck.js
+++ components/IndividualDeck.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Platform,
-  TouchableOpacity,
-} from 'react-native';
+import { View, Text, StyleSheet, Platform, Button } from 'react-native';
 
 export default class IndividualDeck extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -15,20 +9,40 @@ export default class IndividualDeck extends Component {
   };
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <View>
-          <Text>udacicards</Text>
-          <Text>{3} cards</Text>
+          <Text style={styles.deckName}>udacicards</Text>
+          <Text style={styles.numberOfCards}>{3} cards</Text>
         </View>
         <View>
-          <TouchableOpacity>
-            <Text>Add Card</Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Start Quiz</Text>
-          </TouchableOpacity>
+          <View style={styles.buttonContainer}>
+            <Button title="Add Card" onPress={() => {}} />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button title="Start Quiz" onPress={() => {}} />
+          </View>
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    margin: 10,
+  },
+  deckName: {
+    fontSize: 24,
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  numberOfCards: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
