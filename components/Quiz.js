bd6e7507255f155d2d98cedf8a2ce41edbbb48a1
Timothy Moore <moore.tim@outlook.com>
2017-10-19 05:53:49 -0400

    refactor: Change from navigationActions for quiz to FlatList and Modal

--- components/Quiz.js
+++ components/Quiz.js
@@ -7,35 +7,15 @@ import {
   ScrollView,
   TouchableOpacity,
   Button,
+  FlatList,
+  Modal,
+  TouchableHighlight,
 } from 'react-native';
 import Card from './Card';
 import { DrawerNavigator, DrawerItems } from 'react-navigation';
 import QuizCardDrawer from './QuizCardDrawer';
 import { connect } from 'react-redux';
-
-const createCardStackDrawer = (cardsInDeck, deck) => {
-  const navConfig = cardsInDeck.reduce((config, card, index) => {
-    const cardPosition = index + 1;
-    config[`Card${index + 1}`] = {
-      screen: props => (
-        <Card
-          {...props}
-          deckLength={cardsInDeck.length}
-          cardPosition={cardPosition}
-          deckId={deck.id}
-        />
-      ),
-    };
-    return config;
-  }, {});
-  return DrawerNavigator(navConfig, {
-    contentComponent: props => (
-      <ScrollView>
-        <QuizCardDrawer {...props} deckId={deck.id} />
-      </ScrollView>
-    ),
-  });
-};
+import { setCurrentQuestion } from '../actions';
 
 class Quiz extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -43,22 +23,95 @@ class Quiz extends Component {
       title: navigation.state.params.name,
     };
   };
+
+  state = {
+    modalOpen: false,
+  };
+
+  setModalOpen = open => {
+    this.setState({ modalOpen: open });
+  };
+
   render() {
     const { deck, cardsInDeck } = this.props;
-    const Nav = createCardStackDrawer(cardsInDeck, deck);
-    return <Nav />;
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={cardsInDeck}
+          keyExtractor={item => item.id}
+          renderItem={({ item, index }) => (
+            <Card
+              deckLength={cardsInDeck.length}
+              deckId={deck.id}
+              card={item}
+              cardNumber={index + 1}
+            />
+          )}
+          ref={ref => {
+            this.flatListRef = ref;
+          }}
+          horizontal={true}
+          pagingEnabled={true}
+        />
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalOpen}
+          //onRequestClose={() => {
+        >
+          <View style={{ marginTop: 22 }}>
+            <View>
+              {cardsInDeck.map((card, index) => {
+                return (
+                  <TouchableOpacity
+                    onPress={() => {
+                      this.props.setCurrentQuestion(index + 1);
+                      this.flatListRef.scrollToIndex({ index });
+                      this.setModalOpen(!this.state.modalOpen);
+                    }}
+                    key={index}
+                  >
+                    <Text>{`Card ${index + 1}`}</Text>
+                  </TouchableOpacity>
+                );
+              })}
+
+              <TouchableOpacity
+                onPress={() => {
+                  this.setModalOpen(!this.state.modalOpen);
+                }}
+              >
+                <Text>Hide Modal</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalOpen(true);
+          }}
+        >
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+      </View>
+    );
   }
 }
 
-const mapStateToProps = ({ decks, cards }, { navigation }) => {
+const mapStateToProps = ({ quiz, decks, cards }, { navigation }) => {
   const { deckId } = navigation.state.params;
   return {
     deck: decks[deckId],
     cardsInDeck: cards[deckId],
+    currentQuestion: quiz.currentQuestion,
   };
 };
 
-export default connect(mapStateToProps)(Quiz);
+const mapDispatchToProps = dispatch => ({
+  setCurrentQuestion: position => dispatch(setCurrentQuestion(position)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quiz);
 
 const styles = StyleSheet.create({
   container: {
