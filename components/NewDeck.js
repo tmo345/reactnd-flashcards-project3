f0a6e01381d58445d8c08cda6145c2f1c4f014f2
Timothy Moore <moore.tim@outlook.com>
2017-10-20 00:14:32 -0400

    style: Change layout/styles and add message for successfully adding deck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -7,7 +7,9 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   TouchableOpacity,
+  Button,
   KeyboardAvoidingView,
+  Alert,
 } from 'react-native';
 import { connect } from 'react-redux';
 import { addNewDeck } from '../actions';
@@ -15,9 +17,18 @@ import { addNewDeck } from '../actions';
 class NewDeck extends Component {
   state = {
     deckTitle: '',
+    displayFormSuccessMessage: false,
+  };
+
+  submitNewDeck = text => {
+    this.props.dispatch(addNewDeck(this.state.deckTitle));
+    this.setState({ deckTitle: '', displayFormSuccessMessage: true });
+    Keyboard.dismiss();
+    setTimeout(() => this.setState({ displayFormSuccessMessage: false }), 2000);
   };
 
   render() {
+    const { deckTitle, displayFormSuccessMessage } = this.state;
     return (
       /**
        * For how to hide keyboard on touching outside of TextInput, answers had examples
@@ -25,24 +36,35 @@ class NewDeck extends Component {
        * onPress
        * https://stackoverflow.com/questions/29685421/react-native-hide-keyboard
        */
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <KeyboardAvoidingView style={styles.container}>
-          <Text>What is the title of your new deck?</Text>
-          <TextInput
-            onChangeText={text => {
-              console.log(text);
-              this.setState({ deckTitle: text });
-            }}
-            style={styles.textInput}
-            placeholder="Deck Title"
-            value={this.state.deckTitle}
-          />
-          <TouchableOpacity
-            onPress={() =>
-              this.props.dispatch(addNewDeck(this.state.deckTitle))}
-          >
-            <Text>Submit</Text>
-          </TouchableOpacity>
+      <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView behavior="position" style={styles.container}>
+          {displayFormSuccessMessage && (
+            <Text style={{ color: 'green' }}>Deck successfully added</Text>
+          )}
+          <View style={styles.inputContainer}>
+            <Text style={styles.formText}>
+              What is the title of your new deck?
+            </Text>
+            <TextInput
+              onChangeText={text => {
+                this.setState({ deckTitle: text });
+              }}
+              style={styles.textInput}
+              value={deckTitle}
+              onSubmitEditing={() => {
+                if (deckTitle.length > 0) {
+                  this.submitNewDeck();
+                }
+              }}
+            />
+            <Button
+              style={styles.submitButton}
+              disabled={deckTitle.length === 0}
+              onPress={this.submitNewDeck}
+              title="Add Deck"
+              color="#2884CB"
+            />
+          </View>
         </KeyboardAvoidingView>
       </TouchableWithoutFeedback>
     );
@@ -56,10 +78,27 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
   textInput: {
-    width: 100,
-    borderColor: '#222',
-    borderWidth: StyleSheet.hairlineWidth,
+    //width: 200,
+    alignSelf: 'stretch',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 5,
+    marginBottom: 10,
+  },
+  formText: {
+    fontSize: 18,
+    paddingBottom: 20,
+  },
+  inputContainer: {
+    alignItems: 'flex-start',
+    marginBottom: 100,
+  },
+  submitButton: {},
+  submitButtonText: {
+    color: '#fff',
   },
 });
