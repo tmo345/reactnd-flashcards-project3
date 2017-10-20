e01919505349b706a39209b9db29d07003b3fdd7
Timothy Moore <moore.tim@outlook.com>
2017-10-20 12:20:11 -0400

    Style: Add onFocus styles to NewDeck view and change layout

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -18,6 +18,7 @@ class NewDeck extends Component {
   state = {
     deckTitle: '',
     displayFormSuccessMessage: false,
+    deckTitleFocused: false,
   };
 
   submitNewDeck = text => {
@@ -26,6 +27,9 @@ class NewDeck extends Component {
     Keyboard.dismiss();
     setTimeout(() => this.setState({ displayFormSuccessMessage: false }), 2000);
   };
+
+  onDeckTitleFocus = () => this.setState({ deckTitleFocused: true });
+  onDeckTitleBlur = () => this.setState({ deckTitleFocused: false });
 
   render() {
     const { deckTitle, displayFormSuccessMessage } = this.state;
@@ -37,35 +41,38 @@ class NewDeck extends Component {
        * https://stackoverflow.com/questions/29685421/react-native-hide-keyboard
        */
       <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
-        <KeyboardAvoidingView behavior="position" style={styles.container}>
+        <View style={styles.inputContainer}>
           {displayFormSuccessMessage && (
             <Text style={{ color: 'green' }}>Deck successfully added</Text>
           )}
-          <View style={styles.inputContainer}>
-            <Text style={styles.formText}>
-              What is the title of your new deck?
-            </Text>
-            <TextInput
-              onChangeText={text => {
-                this.setState({ deckTitle: text });
-              }}
-              style={styles.textInput}
-              value={deckTitle}
-              onSubmitEditing={() => {
-                if (deckTitle.length > 0) {
-                  this.submitNewDeck();
-                }
-              }}
-            />
-            <Button
-              style={styles.submitButton}
-              disabled={deckTitle.length === 0}
-              onPress={this.submitNewDeck}
-              title="Add Deck"
-              color="#2884CB"
-            />
-          </View>
-        </KeyboardAvoidingView>
+          <Text style={styles.formText}>
+            What is the title of your new deck?
+          </Text>
+          <TextInput
+            onChangeText={text => {
+              this.setState({ deckTitle: text });
+            }}
+            style={[
+              styles.textInput,
+              this.state.deckTitleFocused && styles.focusedStyle,
+            ]}
+            value={deckTitle}
+            onFocus={this.onDeckTitleFocus}
+            onBlur={this.onDeckTitleBlur}
+            onSubmitEditing={() => {
+              if (deckTitle.length > 0) {
+                this.submitNewDeck();
+              }
+            }}
+          />
+          <Button
+            style={styles.submitButton}
+            disabled={deckTitle.length === 0}
+            onPress={this.submitNewDeck}
+            title="Add Deck"
+            color="#2884CB"
+          />
+        </View>
       </TouchableWithoutFeedback>
     );
   }
@@ -81,24 +88,40 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   textInput: {
-    //width: 200,
     alignSelf: 'stretch',
     borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 3,
-    padding: 5,
+    padding: 7.5,
     marginBottom: 10,
+    fontSize: 16,
+    backgroundColor: '#fff',
   },
   formText: {
     fontSize: 18,
     paddingBottom: 20,
   },
   inputContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    marginBottom: 100,
+    paddingTop: 50,
+    paddingBottom: 10,
+    padding: 20,
+    backgroundColor: '#fff',
   },
   submitButton: {},
   submitButtonText: {
     color: '#fff',
+  },
+  focusedStyle: {
+    borderColor: '#84ACCB',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: '#2884CB',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
   },
 });
