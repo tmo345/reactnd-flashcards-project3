18a498978b26fe3b7df309ec01dbb4b6d990ab9b
Timothy Moore <moore.tim@outlook.com>
2017-10-21 13:27:58 -0400

    feat: Use redux-thunk middleware

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ import Quiz from './components/Quiz';
 import { Constants } from 'expo';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import devToolsEnhancer from 'remote-redux-devtools';
@@ -126,15 +127,13 @@ const StackNav = StackNavigator(
   },
 );
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider
-        store={createStore(
-          reducer,
-          window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__(),
-        )}
+        store={createStore(reducer, composeEnhancers(applyMiddleware(thunk)))}
       >
         <View style={styles.container}>
           <FlashCardsStatusBar
