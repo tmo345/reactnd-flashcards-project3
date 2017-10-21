ed44a0c14838fc75351c0f740bb08b1ad24200f3
Timothy Moore <moore.tim@outlook.com>
2017-10-20 21:35:08 -0400

    refactor: Change to click any where on the FormStatusMessage to dismiss

--- components/FormSuccessMessage.js
+++ components/FormSuccessMessage.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const FormSuccessMessage = props => {
   const { submittedItem, dismissFormSuccessMessage } = props;
   return (
-    <View
+    <TouchableOpacity
       style={{
         flexDirection: 'row',
         justifyContent: 'flex-start',
         alignItems: 'center',
       }}
+      onPress={dismissFormSuccessMessage}
     >
       <Ionicons
         name="ios-close"
         size={30}
         color="green"
-        onPress={dismissFormSuccessMessage}
         style={{ padding: 10 }}
       />
       <Text style={{ color: 'green', marginLeft: 7.5 }}>
         {submittedItem} successfully added
       </Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
