e16fa40f72806d63928349042a97b730ed2ad695
Timothy Moore <moore.tim@outlook.com>
2017-10-24 14:31:11 -0400

    fix: Add missing properties to the backfilled card state

--- utils/_decks.js
+++ utils/_decks.js
@@ -10,6 +10,9 @@ export const dummyData = {
         id: 'id1',
         question: 'Are mashed potatoes awesome or amazing?',
         answer: 'Trick question! They are both awesome AND amazing!',
+        answeredCategory: 'unanswered',
+        onQuestionSide: true,
+        answered: false,
       },
       {
         id: 'id2',
@@ -17,6 +20,9 @@ export const dummyData = {
           "True or False: A twice baked potato, or potato boat, is a fancy way of serving mashed potatoes without having to say we're having mashed potatoes",
         answer:
           "Very very true. Also accept answer: Who cares? At least we're having mashed potatoes for dinner!",
+        answeredCategory: 'unanswered',
+        onQuestionSide: true,
+        answered: false,
       },
     ],
   },
