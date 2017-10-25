0674c16857bf07d01c26df212f9cc54e0c5ac27f
Timothy Moore <moore.tim@outlook.com>
2017-10-24 23:59:34 -0400

    Style: Change styles spacing/colors

--- components/QuizResults.js
+++ components/QuizResults.js
@@ -72,22 +72,32 @@ class QuizResults extends Component {
       dispatch,
     } = this.props.navigation.state.params;
     return (
-      <View style={{ marginTop: 22 }}>
-        <View>
-          <Text>Quiz Results for {deck.name}</Text>
+      <View style={styles.container}>
+        <View style={{ marginBottom: 15 }}>
+          <Text style={styles.resultsHeading}>Deck:</Text>
+          <Text style={styles.resultsText}>{deck.name}</Text>
         </View>
-        <View>
-          <Text>
-            {numberCorrect} correct / {cardsInDeck.length} total cards ={' '}
-            {numberCorrect / cardsInDeck.length * 100}%
+
+        <View style={{ marginTop: 15, marginBottom: 15 }}>
+          <Text style={styles.resultsHeading}>Score:</Text>
+          <Text style={styles.resultsText}>
+            {numberCorrect} correct / {cardsInDeck.length} card{cardsInDeck.length === 1 ? '' : 's'}{' '}
+            total = {numberCorrect / cardsInDeck.length * 100}%
           </Text>
         </View>
-        <View>
-          <Button title="Take Quiz Again" onPress={this.retakeQuiz} />
-          <Button
-            title={`Back to ${deck.name} deck`}
+        <View style={{ alignItems: 'flex-start', marginTop: 20 }}>
+          <TouchableOpacity
+            style={styles.navigationButtons}
+            onPress={this.retakeQuiz}
+          >
+            <Text style={styles.navButtonText}>Take Quiz Again</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.navigationButtons}
             onPress={this.backToDeck}
-          />
+          >
+            <Text style={styles.navButtonText}>Back to {deck.name} deck</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -95,3 +105,26 @@ class QuizResults extends Component {
 }
 
 export default connect()(withNavigation(QuizResults));
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingTop: 30,
+  },
+  navigationButtons: {
+    padding: 10,
+    marginBottom: 15,
+  },
+  navButtonText: {
+    color: '#3B62F7',
+    fontSize: 18,
+  },
+  resultsHeading: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  resultsText: {
+    fontSize: 16,
+  },
+});
